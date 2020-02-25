e8e6ea36d455bb5277cda64e61841f0ccd41e56b
leamuz <semprefesta146@gmail.com>
2020-02-25 10:39:32 +0100

    added product-one page

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,7 @@ $(function(){
         slidesToScroll: 4
       });
 
-      $(".js-range-slider").ionRangeSlider({
+    $(".js-range-slider").ionRangeSlider({
         type: "double",
         min: 0,
         max: 1000,
@@ -32,6 +32,15 @@ $(function(){
       $('.icon-th-large').addClass('active');
       $('.icon-th-list').removeClass('active')
     });
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
     $('.menu__btn').on('click', function(){
       $('.menu__list').slideToggle();
